c76fc10df1b2d118b4f222950b2aa106670ed1bd
Harsh Singh <you@example.com>
2025-10-19 21:32:29 +0530

    Changed backend message

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello from Samvaad");
+  res.send("Samvaad Backend Running");
 });
 
 const PORT = process.env.PORT || 5000;
